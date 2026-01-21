74e4ad567a0ba3b8297cda5b34917d4ac801c0cd
Peter Garrett <iixx@Mac.fritz.box>
2026-01-21 18:20:10 +0100

    Fix: Add suppressHydrationWarning to body to resolve hydration mismatch

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${outfit.variable} ${inter.variable}`}>
+      <body className={`${outfit.variable} ${inter.variable}`} suppressHydrationWarning>
         {children}
       </body>
     </html>
